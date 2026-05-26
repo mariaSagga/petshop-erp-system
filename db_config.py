87fd68eae2825ad6ea721e4dcf34580daf507e55
mariaSagga <mariasagga99@gmail.com>
2026-05-26 16:54:47 +0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,11 +1,13 @@
-import mysql.connector
+import psycopg2
+
 def get_db_connection():
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
+
+    connection = psycopg2.connect(
+        host="dpg-d8aq85f7f7vs73ddg9f0-a",
         database="erp_ecommerce",
+        user="petshop_user",
+        password="fOzoicXlrsNZG5fZaEq4Gylvh5KX37Ku",
+        port="5432"
     )
+
     return connection
-
-
