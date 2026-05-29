b160d6276b8e019d0ee75e991bacb5e33ec61da9
mariaSagga <mariasagga99@gmail.com>
2026-05-29 13:35:14 +0300

    Update db_config.py

--- database/db_config.py
+++ database/db_config.py
@@ -3,9 +3,9 @@ import os
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        database=os.getenv("DB_NAME", "erp_ecommerce")
     )
     return connection
